048a96fc30b01c56a94f0621d8cd8b9e12d29057
Arguelless <marguelles@protonmail.com>
2023-09-26 16:24:23 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ex1.2_endevina_el_numero_arguelles"
+include(":app")
+ 
